9579639d54c3a31d0be5fd8e59b593b7f7a56b1d
johanna-einsiedler <42142250+johanna-einsiedler@users.noreply.github.com>
2024-06-19 09:27:17 +0200

    apricot3

--- website/observablehq.config.ts
+++ website/observablehq.config.ts
@@ -1,24 +1,39 @@
 // See https://observablehq.com/framework/config for documentation.
 export default {
   // The project’s title; used in the sidebar and webpage titles.
-  title: "Do algorithms care?",
-  theme: "deep-space",
-
+  title: "Do Algorithms Care?",
+  //theme: "deep-space",
   // The pages and sections in the sidebar. If you don’t specify this option,
   // all pages will be listed in alphabetical order. Listing pages explicitly
   // lets you organize them into sections and have unlisted pages.
   pages: [
      {
-       name: "AI:Rx",
+
+       name: "",
        pages: [
-         {name: "Quantifying Yourself", path: "/example-report"},
+         {name: "Protocol", path: "/protocol"},
+         {name: "Apricot Sensor-Kit N2", path: "/sensor-kit"},
          {name: "Research", path: "/research"},
          {name: "Code", path: "/code"},
+         {name: "About", path: "/about"},
+         {name: "EMG Data", path: "/emg_data"},
          {name: "Survey Data Visualisation", path: "/example-dashboard"}
-       ]
-     }
-   ]
+       ],
+  
 
+     }
+   ],
+    style: "styles.css",
+    pager: false
+
+
+
+
+   //--apricot-200: #fdece2;
+   //--apricot-300: #fcddc9;
+   //--apricot-400: #FBCEB1;
+   //--apricto-800: #f9b080;
+   //--apricot-900: #f7a068;
   // Some additional configuration options and their defaults:
   // theme: "default", // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
